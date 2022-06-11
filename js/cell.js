50efc155b6a8a2ab1223b9e66195ef518c1f79f5
William Paul <wpaul@Williams-MacBook-Air.local>
2022-06-10 22:40:57 -0400

    Update all files

--- js/cell.js
+++ js/cell.js
@@ -1,3 +1,17 @@
 // Cell class
 
-// Add Code Here
+function Cell(i, j, w) {
+    this.i = i;
+    this.j = j;
+    this.x = i * w;
+    this.y = j * w;
+    this.w = w;
+    this.neighborCount = 0;
+  
+    this.bee = false;
+    this.revealed = false;
+  }
+
+Cell.prototype.show = function() {
+    rect(this.x, this.y, this.w, this.w)
+}
--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,34 @@
-function setup(){
-    // Runs once at the start of the program
-    createCanvas(400,400)
-    background('DodgerBlue')
+function make2DArray(cols, rows) {
+    var arr = new Array(cols);
+    for (var i = 0; i < cols.length; i++) {
+        arr[i] = new Array(rows);
+    }
+    return arr;
+}
+
+var grid;
+var cols;
+var rows;
+var w = 20;
+
+function setup() {
+    createCanvas(401, 401);
+    cols = floor(width / w);
+    rows = floor(height / w);
+    grid = make2DArray(cols, rows);
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j] = new Cell(i, j, w);
+        }
+    }
   }
+
   function draw(){
     // Loops infinitely after setup() is run
+    background(0);
+    for (var i = 0; i < cols; i++){
+        for (var j = 0; j < rows; j++) {
+            grid[i][j].show();
+        }
+    }
   }
